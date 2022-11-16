4f694ee547871104f9622a29caa1d5115d3bccc5
HONG-LOU <3264477647@qq.com>
2022-11-16 21:05:56 +0800

    d.cpp

---
+++ Daily_Training/11/16/d.cpp
@@ -0,0 +1,12 @@
+/**
+ *    author:  HONG-LOU
+ *    created: 2022-11-16 21:03:19
+**/
+#include <bits/stdc++.h>
+
+auto main() -> int {
+  std::cin.tie(nullptr) -> std::ios::sync_with_stdio(false);
+
+  std::cout << "Today's all test is very successful, vscode, md, git, cpp, nice!" << std::endl;
+  return 0;
+}
