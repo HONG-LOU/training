594a84428cdde3ba5279124b9fdf90aebc846eb9
HONG-LOU <3264477647@qq.com>
2022-11-16 19:28:14 +0800

    dijkstra.cpp

---
+++ Daily_Training/11/16/b.cpp
@@ -0,0 +1,45 @@
+/**
+ *    author:  HONG-LOU
+ *    created: 2022-11-16 19:23:39
+**/
+#include <bits/stdc++.h>
+
+auto main() -> int {
+  std::cin.tie(nullptr) -> std::ios::sync_with_stdio(false);
+
+  int n, m, s;
+  std::cin >> n >> m >> s;
+  std::vector<long long> dis(n, (1ll << 31) - 1);
+  dis[s - 1] = 0;
+  std::vector<std::vector<std::array<int, 2>>> g(n);
+  for (int i = 0; i < m; i++) {
+    int u, v, w;
+    std::cin >> u >> v >> w;
+    u--, v--;
+    g[u].push_back({v, w});
+  }
+  std::function<void()> dijkstra = [&] () {
+    std::priority_queue<std::pair<long long, int>, std::vector<std::pair<long long, int>>, std::greater<>> h;
+    for (int i = 0; i < n; i++) {
+      h.emplace(dis[i], i);
+    }
+    while (!h.empty()) {
+      auto [d, x] = h.top();
+      h.pop();
+      if (d > dis[x]) {
+        continue;
+      }
+      for (auto [y, w] : g[x]) {
+        if (d + w < dis[y]) {
+          dis[y] = d + w;
+          h.emplace(dis[y], y);
+        }
+      }
+    }
+  };
+  dijkstra();
+  for (int i = 0; i < n; i++) {
+    std::cout << dis[i] << " \n"[i == n - 1];
+  }
+  return 0;
+}
