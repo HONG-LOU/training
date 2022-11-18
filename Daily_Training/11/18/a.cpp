14683edf116638d6a94ccfd5712d147e1b78c21e
HONG-LOU <3264477647@qq.com>
2022-11-18 21:08:18 +0800

    changes

---
+++ Daily_Training/11/18/a.cpp
@@ -0,0 +1,12 @@
+/**
+ *    author:  HONG-LOU
+ *    created: 2022-11-18 19:13:05
+**/
+#include <bits/stdc++.h>
+
+auto main() -> int {
+  std::cin.tie(nullptr) -> std::ios::sync_with_stdio(false);
+
+  
+  return 0;
+}
