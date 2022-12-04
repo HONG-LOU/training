44db430c2b1d805c4bbf52b3465a05fdc9275c7f
HONG-LOU <3264477647@qq.com>
2022-12-04 17:31:35 +0800

    12-4

---
+++ Daily_Training/12/4/a.cpp
@@ -0,0 +1,12 @@
+/**
+ *    author:  HONG-LOU
+ *    created: 2022-12-04 15:47:40
+**/
+#include <bits/stdc++.h>
+
+auto main() -> int {
+  std::cin.tie(nullptr) -> std::ios::sync_with_stdio(false);
+
+  printf("%.10f", 2.0 / 40);  
+  return 0;
+}
