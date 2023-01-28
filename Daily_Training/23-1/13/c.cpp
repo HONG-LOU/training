28f2a909cc1f6b933af5e13eb478c3eaa33ba3a5
hong-lou <3264477647@qq.com>
2023-01-28 13:56:21 +0800

    23-1-28-test

---
+++ Daily_Training/23-1/13/c.cpp
@@ -0,0 +1,14 @@
+/**
+ *    author:  HONG-LOU
+ *    created: 2023-01-28 13:54:09
+**/
+#include <bits/stdc++.h>
+
+auto main () -> int {
+  std::cin.tie(nullptr) -> std::ios::sync_with_stdio(false);
+
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << "sum is " << a + b << "\n";
+  return 0;
+}
