7411c6117015e17fb5fdf6c169eebd140b758dc7
hong-lou <3264477647@qq.com>
2023-10-03 15:48:09 +0800

    10

---
+++ Daily_Training/23-10/1/a.cpp
@@ -0,0 +1,12 @@
+/**
+ *    author:  HONG-LOU
+ *    created: 2023-10-03 15:46:33
+**/
+#include <bits/stdc++.h>
+
+auto main () -> int {
+  std::cin.tie(nullptr) -> std::ios::sync_with_stdio(false);
+
+  std::cout << "hello world!" << std::endl;
+  return 0;
+}
